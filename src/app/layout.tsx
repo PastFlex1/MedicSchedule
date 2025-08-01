f43c6e13b0993673ab8ccb4a527a65fe8f8a154f
Alex <past6867@gmail.com>
2025-08-01 01:03:37 -0500

    icono actualizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,10 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import * as React from 'react';
 
 export const metadata: Metadata = {
   title: 'MediSchedule',
   description: 'Sistema de Gestión de Citas Médicas',
-  icons: {
-    icon: 'https://i.pinimg.com/736x/70/93/0b/70930b2e4a4a3cfa2174a2a0b8ee4999.jpg',
-  },
 };
 
 export default function RootLayout({
@@ -20,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="es" className="scroll-smooth">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
